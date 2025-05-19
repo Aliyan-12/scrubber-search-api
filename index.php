f496418e8eb68f206a484840d7816717749af53c
Aliyan-12 <alyannasir225@gmail.com>
2025-05-20 03:01:07 +0500

    added .htaccess config.

--- index.php
+++ index.php
@@ -1,21 +1,6 @@
 <?php
 
-$data = [
-    'phone' => '8089201930',
-    'campaign' => 'ACA'
-];
+$response['message'] = 'This is a scrubber api index and cannot be accessed from browser.';
 
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, 'http://127.0.0.1:8000/api/search.php');
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, [
-    'Content-Type: application/json'
-]);
-
-$response = curl_exec($ch);
-$http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-
-print_r(json_decode($response));
+print_r(json_encode($response));
 exit;
